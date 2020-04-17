e96a362632732f7939245322632a3a20029ca8b3
卷毛 <zephyrtang@aliyun.com>
2020-04-18 01:27:58 +0800

    feat : test

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import logo from '../logo.svg';
 import { Link } from 'react-router-dom';
-import Accordion from './Accordion';
+// import Accordion from './Accordion';
 
 const fetch = (host) => {
   axios.get('/api/hello').then(res => {
@@ -18,9 +18,9 @@ const Home = () => {
       <Link to='/test'>go to test page</Link>
       <Link to='/test2'>go to test test  page2.</Link>
       <button onClick={fetch}>click me to fetch</button>
-      <Accordion>
+      {/* <Accordion>
         测试字数测试紫薯测试字数测试紫薯测试字数测试紫薯测试字数测试紫薯测试字数测试紫薯测试字数测试紫薯测试字数测试紫薯测试字数测试紫薯测试字数测试紫薯
-      </Accordion>
+      </Accordion> */}
     </div>
   );
 }
